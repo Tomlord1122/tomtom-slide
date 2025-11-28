bfa2e48275a8acf142738b8d33342fa254312d40
Tom Liu (LIU HSIU-CHI) <tom_liu@trendmicro.com>
2025-11-28 15:54:48 +0800

    init the project
    Signed-off-by: Tom Liu (LIU HSIU-CHI) <tom_liu@trendmicro.com>

--- slidev-deck-explorer/index.tsx
+++
@@ -1,4 +0,0 @@
-// This file is disabled because the application is now running as a standalone 
-// vanilla JavaScript application in index.html, satisfying the "no framework" 
-// and "single file" requirement.
-console.log("Running in standalone mode.");
